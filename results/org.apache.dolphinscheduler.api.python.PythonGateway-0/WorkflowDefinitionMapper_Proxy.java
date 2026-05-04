aa0e5d9e3a0c47964ca0a0df3429c6cb13087312
Federico Valeri <federico.valeri@unicam.it>
2026-05-04 11:50:30 +0200

    added first result for new logic

---
+++ results/org.apache.dolphinscheduler.api.python.PythonGateway-0/WorkflowDefinitionMapper_Proxy.java
@@ -0,0 +1,37 @@
+package org.apache.dolphinscheduler.api.python;
+
+import org.apache.dolphinscheduler.api.service.ResourcesService;
+import org.apache.dolphinscheduler.dao.entity.Project;
+import org.apache.dolphinscheduler.dao.entity.TaskDefinition;
+import org.apache.dolphinscheduler.dao.entity.User;
+import org.apache.dolphinscheduler.dao.entity.WorkflowDefinition;
+import org.apache.dolphinscheduler.dao.mapper.ProjectMapper;
+import org.apache.dolphinscheduler.dao.mapper.TaskDefinitionMapper;
+import org.apache.dolphinscheduler.dao.mapper.WorkflowDefinitionMapper;
+import org.apache.dolphinscheduler.plugin.storage.api.StorageEntity;
+import org.apache.dolphinscheduler.spi.enums.ResourceType;
+
+import java.util.Date;
+import java.util.Map;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+
+class WorkflowDefinitionMapper_Proxy extends WorkflowDefinitionMapper_EmptyProxy {
+    public WorkflowDefinitionMapper_Proxy(WorkflowDefinitionMapper dependency) {
+        super(dependency);
+    }
+
+    public WorkflowDefinition queryByDefineName(long projectCode, String workflowDefinitionName) {
+        WorkflowDefinition result = dependency.queryByDefineName(projectCode, workflowDefinitionName);
+        Assertions.assertNotNull(result);
+        return result;
+    }
+}
