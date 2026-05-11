4b399e61575b95191d47e6debe90cfcc03b9863a
Federico Valeri <federico.valeri@unicam.it>
2026-05-11 12:06:01 +0200

    fix system_prompt_for_integrationTest.md for explicit mock declaration

---
+++ gen/com/alibaba/nacos/client/naming/remote/http/NamingHttpClientManagerIntegrationTest_testShutdown.java
@@ -0,0 +1,38 @@
+package com.alibaba.nacos.client.naming.remote.http;
+
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.common.http.HttpClientBeanHolder;
+import com.alibaba.nacos.common.http.client.NacosRestTemplate;
+import com.alibaba.nacos.common.http.client.request.HttpClientRequest;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.Map;
+
+import static com.alibaba.nacos.common.constant.RequestUrlConstants.HTTP_PREFIX;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+
+
+class NamingHttpClientManagerIntegrationTest_testShutdown {
+
+@Test
+    void testShutdown() throws NoSuchFieldException, IllegalAccessException, NacosException, IOException {
+        NamingHttpClientManager instance = NamingHttpClientManager.getInstance();
+
+        HttpClientRequest mockHttpClientRequest = Mockito.mock(HttpClientRequest.class);
+        HttpClientRequest mockHttpClientRequest_proxy = new HttpClientRequest_Proxy(<--HttpClientRequest_REAL_CONFIGURED_INSTANCE-->);
+        Field requestClient = NacosRestTemplate.class.getDeclaredField("requestClient");
+        requestClient.setAccessible(true);
+        requestClient.set(instance.getNacosRestTemplate(), mockHttpClientRequest_proxy);
+        NamingHttpClientManager.getInstance().shutdown();
+        assertEquals(1, ((HttpClientRequest_Proxy) mockHttpClientRequest_proxy).close_verify());
+    }
+    }
